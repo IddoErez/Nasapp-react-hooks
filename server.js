4a1354875e6cd8b5c9bb3bd0d0ee9707fce9070f
Ido Erez <idodoerez@gmail.com>
2021-02-01 00:32:06 +0200

    reay to deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const api = require('./server/routes/api')
 const mongoose = require('mongoose')
 const path = require('path')
-mongoose.connect('mongodb://localhost/picturesDB', { useNewUrlParser: true},  { useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/transactionsDB')
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
@@ -16,7 +16,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', api)
 
-const port = 3200
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3200
 app.listen(port, function () {
     console.log(`Running on port ${port}`)
 })
+
