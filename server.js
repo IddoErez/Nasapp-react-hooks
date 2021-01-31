33e6e2bf4626f7a8fab51644b688ccd507bed49f
Ido Erez <idodoerez@gmail.com>
2021-02-01 01:08:01 +0200

    123

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
-const api = require('./server/Routes/api')
+const api = require('./server/routes/api')
 const mongoose = require('mongoose')
 const path = require('path')
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/picturesDB')
 
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-//     next()
-// })
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
