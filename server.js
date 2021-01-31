1bc3647f9a8ed0f13517da4d1437adb561bf1726
Ido Erez <idodoerez@gmail.com>
2021-02-01 01:30:06 +0200

    123

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const mongoose = require('mongoose')
 const path = require('path')
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/picturesDB')
 
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+//     next()
+// })
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -14,7 +20,7 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 3200
+const port = process.env.PORT || 4500
 app.listen(port, function () {
     console.log(`Running on port ${port}`)
 })
