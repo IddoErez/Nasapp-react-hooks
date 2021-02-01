b81b7d451c10ec6f83e15ab91d0c3b7a67799702
Ido Erez <idodoerez@gmail.com>
2021-02-01 13:10:18 +0200

    Ready

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+// const api = require('./server/routes/api')
 const api = require('./server/routes/api')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
