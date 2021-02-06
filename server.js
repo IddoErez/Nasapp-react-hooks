5c36a155791005a026bf1162eba2293fa82c020a
Ido Erez <idodoerez@gmail.com>
2021-02-06 19:52:29 +0200

    www

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-// const api = require('./server/routes/api')
+const api = require('./Server/routes/api')
 const mongoose = require('mongoose')
 const path = require('path')
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/picturesDB')
@@ -14,7 +14,7 @@ mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/picturesDB')
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use('/', api)
+app.use('/', api)
 
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
